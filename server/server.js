a1701c5cbb643f17e663f78e1e9691a4f30d2cff
Aliyan-debug <mynameisaliyan4@gmail.com>
2026-07-31 01:18:13 +0500

    fix: wrap top-level await for cPanel Passenger compatibility

--- server/server.js
+++ server/server.js
@@ -2,13 +2,17 @@ import app from './src/app.js'
 import { env } from './src/config/env.js'
 import { connectDB } from './src/config/db.js'
 
-await connectDB()
+async function start() {
+  await connectDB()
 
-const server = app.listen(env.port, () => {
-  console.log(`✅ Server running in ${env.nodeEnv} mode on port ${env.port}`)
-})
+  const server = app.listen(env.port, () => {
+    console.log(`✅ Server running in ${env.nodeEnv} mode on port ${env.port}`)
+  })
 
-process.on('unhandledRejection', (err) => {
-  console.error(`Unhandled Rejection: ${err.message}`)
-  server.close(() => process.exit(1))
-})
+  process.on('unhandledRejection', (err) => {
+    console.error(`Unhandled Rejection: ${err.message}`)
+    server.close(() => process.exit(1))
+  })
+}
+
+start()
